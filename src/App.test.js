ad75dd8f6dad58a444f610290f8d88f13a6e741d
Nelson R. Graña <87838742+nelsongraa8@users.noreply.github.com>
2022-03-29 12:18:28 +0200

    Crear un unit test simple pero suficiente

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn react link 1', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/azul/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link 2', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/amarillo/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link 3', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/verde/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link 4', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/rojo/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link 5', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Random/i);
   expect(linkElement).toBeInTheDocument();
 });
